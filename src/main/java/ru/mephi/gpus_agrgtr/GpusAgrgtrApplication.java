7933398c08c65c4f13ffc1781288f240f07512d6
SlaykSD <60920077+SlaykSD@users.noreply.github.com>
2022-11-20 10:43:07 +0300

    fix naming

--- src/main/java/ru/mephi/gpus_agrgtr/GpusAgrgtrApplication.java
+++ src/main/java/ru/mephi/gpus_agrgtr/GpusAgrgtrApplication.java
@@ -1,8 +1,10 @@
 package ru.mephi.gpus_agrgtr;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@SpringBootApplication
 @EnableEurekaClient
 public class GpusAgrgtrApplication{
 
