4b481039cbeba0423c5da57ba527652751f72ef5
grigory <grigory.bashev@yandex.ru>
2022-12-11 20:00:01 +0300

    Added DateUtilsImpl to context

--- src/main/java/ru/mephi/gpus_agrgtr/utils/DateUtilsImpl.java
+++ src/main/java/ru/mephi/gpus_agrgtr/utils/DateUtilsImpl.java
@@ -1,8 +1,9 @@
 package ru.mephi.gpus_agrgtr.utils;
 
+import org.springframework.stereotype.Component;
 import java.time.Instant;
 import java.util.Date;
-
+@Component
 public class DateUtilsImpl implements DateUtils {
     @Override
     public Date getNow() {
